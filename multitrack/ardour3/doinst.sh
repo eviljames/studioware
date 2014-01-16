d5a8b0b4bffe42e0d380206b215880f03386ac38
David Woodfall <dave@studioware.org>
2014-01-16 13:35:49 +0000

    Remove capabilities from ardour, ardour3

--- multitrack/ardour3/doinst.sh
+++ multitrack/ardour3/doinst.sh
@@ -24,7 +24,3 @@ if [ -x /usr/bin/gtk-update-icon-cache ] \
   /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
 fi
 
-if [ -x /sbin/setcap ]; then
-    /sbin/setcap cap_ipc_lock,cap_sys_nice=ep usr/bin/ardour3
-fi
-
