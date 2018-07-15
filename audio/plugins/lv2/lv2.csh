f115025f579809777e166d6462b969ed509cb705
slackhead <slackhead@gmail.com>
2018-07-15 21:20:43 +0100

    Fix multiple lv2 paths. Add /etc/profile.d files

---
+++ audio/plugins/lv2/lv2.csh
@@ -0,0 +1,5 @@
+#!/bin/csh
+
+# Add the standard LV2_PATH
+
+setenv LV2_PATH $HOME/.lv2:/usr/local/lib/lv2:/usr/local/lib64/lv2:/usr/lib/lv2:/usr/lib64/lv2
---
+++ audio/plugins/lv2/lv2.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Add the standard LV2_PATH
+
+LV2_PATH=$HOME/.lv2:/usr/local/lib/lv2:/usr/local/lib64/lv2:/usr/lib/lv2:/usr/lib64/lv2
+
+export LV2_PATH
